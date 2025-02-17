e99caf1c6d898cc3e469c237481fff6c8cb10b54
zenitogr <zenitogr@gmail.com>
2025-02-17 11:30:51 +0200

    push push baby!!!

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
@@ -12,7 +11,5 @@ export default defineConfig(({ mode }) => ({
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
+  ],
 }));
